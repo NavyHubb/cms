e06dc6fd7aa6b2c3ed3e857fb9218ddefc95a0b6
NavyHubb <sngjae972@gmail.com>
2023-03-27 13:53:09 +0900

    feat : [010-Product-modify] Product update, ProductItem update

--- order-api/src/main/java/com/zerobase/cms/order/controller/SellerProductController.java
+++ order-api/src/main/java/com/zerobase/cms/order/controller/SellerProductController.java
@@ -1,10 +1,7 @@
 package com.zerobase.cms.order.controller;
 
 import com.zerobase.cms.order.domain.model.ProductItem;
-import com.zerobase.cms.order.domain.product.AddProductForm;
-import com.zerobase.cms.order.domain.product.AddProductItemForm;
-import com.zerobase.cms.order.domain.product.ProductDto;
-import com.zerobase.cms.order.domain.product.ProductItemDto;
+import com.zerobase.cms.order.domain.product.*;
 import com.zerobase.cms.order.service.ProductItemService;
 import com.zerobase.cms.order.service.ProductService;
 import com.zerobase.domain.config.JwtAuthenticationProvider;
@@ -25,14 +22,28 @@ public class SellerProductController {
     @PostMapping
     public ResponseEntity<ProductDto> addProduct(@RequestHeader("X-AUTH-TOKEN") String token,
                                                  @RequestBody AddProductForm form) {
-        ProductDto productDto = ProductDto.from(productService.addProduct(provider.getUserVo(token).getId(), form));
-        return ResponseEntity.ok(productDto);
+        return ResponseEntity.ok(
+                ProductDto.from(productService.addProduct(provider.getUserVo(token).getId(), form)));
     }
 
     @PostMapping("/item")
     public ResponseEntity<ProductDto> addProductItem(@RequestHeader("X-AUTH-TOKEN") String token,
                                                      @RequestBody AddProductItemForm form) {
-        ProductDto productDto = ProductDto.from(productItemService.addProductItem(provider.getUserVo(token).getId(), form));
-        return ResponseEntity.ok(productDto);
+        return ResponseEntity.ok(
+                ProductDto.from(productItemService.addProductItem(provider.getUserVo(token).getId(), form)));
+    }
+
+    @PutMapping
+    public ResponseEntity<ProductDto> updateProduct(@RequestHeader("X-AUTH-TOKEN") String token,
+                                                 @RequestBody UpdateProductForm form) {
+        return ResponseEntity.ok(
+                ProductDto.from(productService.updateProduct(provider.getUserVo(token).getId(), form)));
+    }
+
+    @PutMapping("/item")
+    public ResponseEntity<ProductItemDto> updateProductItem(@RequestHeader("X-AUTH-TOKEN") String token,
+                                                     @RequestBody UpdateProductItemForm form) {
+        return ResponseEntity.ok(
+                ProductItemDto.from(productItemService.updateProductItem(provider.getUserVo(token).getId(), form)));
     }
 }
