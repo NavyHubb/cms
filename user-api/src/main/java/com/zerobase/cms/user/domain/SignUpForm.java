993338aed364c46b60b2559b158978285db08eb6
NavyHubb <sngjae972@gmail.com>
2023-03-29 18:21:45 +0900

    회원가입(구매자, 판매자) 시 입력값에 대한 유효성 검사 및 예외처리
    - 이메일, 비밀번호, 전화번호

--- user-api/src/main/java/com/zerobase/cms/user/domain/SignUpForm.java
+++ user-api/src/main/java/com/zerobase/cms/user/domain/SignUpForm.java
@@ -1,10 +1,14 @@
 package com.zerobase.cms.user.domain;
 
+import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import java.time.LocalDate;
 
 @Getter
@@ -12,9 +16,21 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SignUpForm {
+    @NotBlank(message = "이메일은 필수 입력값입니다.")
+    @Email(message = "이메일 주소 양식에 맞추어 입력해주세요. ex) green@naver.com")
     private String email;
+
+    @NotBlank(message = "이름은 필수 입력값입니다.")
     private String name;
+
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
+    @NotBlank(message = "비밀번호는 필수 입력값입니다.")
     private String password;
+
+    @NotNull
     private LocalDate birth;
+
+    @NotBlank(message = "전화번호는 필수 입력값입니다.")
+    @Pattern(regexp = "01(?:0|1|[6-9])[.-]?(\\d{3}|\\d{4})[.-]?(\\d{4})$", message = "전화번호 양식에 맞추어 입력해주세요. ex) 010-0000-0000")
     private String phone;
 }
--- user-api/src/main/java/com/zerobase/cms/user/exception/ErrorCode.java
+++ user-api/src/main/java/com/zerobase/cms/user/exception/ErrorCode.java
@@ -14,7 +14,9 @@ public enum ErrorCode {
     NOT_FOUND_USER(HttpStatus.BAD_REQUEST, "존재하지 않는 회원입니다."),
     ALREADY_VERIFIED(HttpStatus.BAD_REQUEST, "이미 인증이 완료되었습니다."),
     LOGIN_CHECK_FAIL(HttpStatus.BAD_REQUEST, "아이디나 패스워드를 확인해주세요. 혹은 아직 인증되지 않은 회원입니다."),
-    NOT_ENOUGH_BALANCE(HttpStatus.BAD_REQUEST, "잔액이 부족합니다.");
+    NOT_ENOUGH_BALANCE(HttpStatus.BAD_REQUEST, "잔액이 부족합니다."),
+    INVALID_VALUE(HttpStatus.BAD_REQUEST, "유효하지 않은 값입니다."),
+    NOT_NULL(HttpStatus.BAD_REQUEST, "null이 아닌 값을 입력해주세요.");
 
     private final HttpStatus httpStatus;
     private final String detail;
